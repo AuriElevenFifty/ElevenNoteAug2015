7cddf970a284518217e634ed0d1c18f3d11b5aef
Auri A. Rahimzadeh <auri@elevenfifty.com>
2015-09-01 15:54:13 -0400

    Added game easter egg. About to start Eleven Note.

---
+++ ElevenNote.Web/Controllers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using ElevenNote.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        #region GET
+
+        // GET: Game
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult IndexGet()
+        {
+            var correctAnswer = new Random().Next(1, 10);
+            Session["Answer"] = correctAnswer;
+            return View();
+        }
+
+        #endregion
+
+        #region POST
+
+        // POST: Game
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(GuessingGameViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Guess == (int)Session["Answer"])
+                {
+                    ViewBag.Win = true;
+                }
+                else
+                {
+                    ViewBag.Win = false;
+                }
+            }
+            return View(model);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required(ErrorMessage = "Required")]
+        [MinLength(2, ErrorMessage = "Too short")]
+        [MaxLength(20, ErrorMessage = "Too long")]
+        [Display(Name = "Your Name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, 10, ErrorMessage = "Must be between 1 and 10.")]
+        [Display(Name = "Your Guess")]
+        public int Guess { get; set; }
+    }
+}
