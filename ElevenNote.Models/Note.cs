dcc07df8382f9056b187b61c519caac57ddb5ae8
Auri A. Rahimzadeh <auri@elevenfifty.com>
2015-09-01 17:48:55 -0400

    Almost done with database layer.

---
+++ ElevenNote.Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Contents { get; set; }
+
+        [Required]
+        public Guid ApplicationUserId { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsFavorite { get; set; }
+    }
+}
